53a9d26d8c78e292c7ceb94b536116fb321fb330
zwq <2024570755@qq.com>
2019-09-10 17:45:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -37,14 +37,13 @@
     $zone = @file_get_contents("http://169.254.169.254/latest/meta-data/placement/availability-zone");
     $private_id = @file_get_contents("http://instance-data/latest/meta-data/local-ipv4");
   ?>
-    <img src="https://assets.unisys.com/PublishingImages/Global/Misc/unisysshareimage.jpg" width="300"/>
+    <img src="https://unisyscorp.sharepoint.com/teams/BrandCenter/PublishingImages/logo_guidelines/UnisysLockupCentered_R.png?ctag=190910" width="300"/>
     </br>    
     <h2>EC2 Instance ID: <?php echo $instance_id; ?></h2>
     <h2>EC2 Zone: <?php echo $zone; ?></h2>
     <h2>EC2 Private IP: <?php echo $private_id; ?></h2>
     </br>
-    <p>Please refer this link for more details. <a href="http://aws.amazon.com/documentation/codedeploy">AWS CodeDeploy Document</a>. Blue: #0188cc, Green: #00cc3a</p>
-    <p>danrongm@amazon.com</a>.</p>
+    <p>Please refer this link for more details. <a href="http://aws.amazon.com/documentation/codedeploy">AWS CodeDeploy Document</a>.</p>
   </div>    
 </body>
 </html>
