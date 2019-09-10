a15d5b1cdc384ff92eeb331ed414ef186ed70a2e
zwq <2024570755@qq.com>
2019-09-10 17:54:58 +0800

    Revert "Update index.php"
    This reverts commit ce202d9849d61d7200f20a04734a62810813da60.

--- index.php
+++ index.php
@@ -35,13 +35,13 @@
   <?php
     $instance_id = @file_get_contents("http://instance-data/latest/meta-data/instance-id");
     $zone = @file_get_contents("http://169.254.169.254/latest/meta-data/placement/availability-zone");
-    $public_id = @file_get_contents("http://instance-data/latest/meta-data/public-ipv4");
+    $private_id = @file_get_contents("http://instance-data/latest/meta-data/local-ipv4");
   ?>
     <img src="https://unisyscorp.sharepoint.com/teams/BrandCenter/PublishingImages/logo_guidelines/UnisysLockupCentered_R.png?ctag=190910" width="300"/>
     </br>    
     <h2>EC2 Instance ID: <?php echo $instance_id; ?></h2>
     <h2>EC2 Zone: <?php echo $zone; ?></h2>
-    <h2>EC2 Public IP: <?php echo $public_id; ?></h2>
+    <h2>EC2 Private IP: <?php echo $private_id; ?></h2>
     </br>
     <p>Please refer this link for more details. <a href="http://aws.amazon.com/documentation/codedeploy">AWS CodeDeploy Document</a>.</p>
   </div>    
