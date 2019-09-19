9d8c6ef4c12c0ecd486b1b32a7ebbd53865e0b72
hanxb66 <xuebin.han@unisys.com>
2019-09-19 13:14:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
     $zone = @file_get_contents("http://169.254.169.254/latest/meta-data/placement/availability-zone");
     $private_id = @file_get_contents("http://instance-data/latest/meta-data/local-ipv4");
   ?>
-    <img src="https://unisyscorp.sharepoint.com/teams/BrandCenter/PublishingImages/logo_guidelines/UnisysLockupCentered_R.png?ctag=190918" width="300"/>
+    <img src="https://desktop.github.com/images/desktop-icon.svg" width="300"/>
     </br>    
     <h2>EC2 Instance ID: <?php echo $instance_id; ?></h2>
     <h2>EC2 Zone: <?php echo $zone; ?></h2>
